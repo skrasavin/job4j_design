17b8fbda862f648c1a4c62f1b320b22c02247e4e
sergey <s-krasssavin@mail.ru>
2021-02-04 18:08:37 +0300

    1. Что такое итератор. [#455313]

--- src/test/java/ru/job4j/iterator/BackwardArrayItTest.java
+++ src/test/java/ru/job4j/iterator/BackwardArrayItTest.java
@@ -16,7 +16,6 @@ public class BackwardArrayItTest {
                 new int[] {1, 2, 3}
         );
         assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));
     }
 
     @Test
