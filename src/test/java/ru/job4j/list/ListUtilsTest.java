9aeb74fbd43e112774d68daf4659e8151cc4963e
sergey <s-krasssavin@mail.ru>
2021-03-11 22:56:50 +0300

    7. ListIterator [#455303]

---
+++ src/test/java/ru/job4j/list/ListUtilsTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.list;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+public class ListUtilsTest {
+
+    @Test
+    public void whenAddBefore() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(Arrays.asList(1, 2, 3), Is.is(input));
+    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddBeforeWithInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 3, 2);
+    }
+
+    @Test
+    public void whenAddAfter() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addAfter(input, 1, 2);
+        assertThat(Arrays.asList(1, 3, 2), Is.is(input));
+    }
+
+    @Test
+    public void whenRemoveElementByPredicate() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ListUtils.removeIf(input, a -> a < 2);
+        assertThat(Arrays.asList(2, 3), Is.is(input));
+    }
+
+    @Test
+    public void whenReplaceElementIf() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ListUtils.replaceIf(input, a -> a == 3, 0);
+        assertThat(Arrays.asList(1, 2, 0), Is.is(input));
+    }
+
+    @Test
+    public void whenRemoveAllElements() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> listCompare = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.removeAll(input, listCompare);
+        assertThat(Collections.singletonList(2), Is.is(input));
+    }
+
+}
