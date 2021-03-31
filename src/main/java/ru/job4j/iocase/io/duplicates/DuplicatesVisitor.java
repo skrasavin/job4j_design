c01e12be8493b2c0ccb830bdd25a6eb19f618b03
sergey <s-krasssavin@mail.ru>
2021-03-31 23:45:17 +0300

    4.2. Поиск дубликатов [#455357]

--- src/main/java/ru/job4j/iocase/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/iocase/io/duplicates/DuplicatesVisitor.java
@@ -1,21 +1,35 @@
 package ru.job4j.iocase.io.duplicates;
 
-import ru.job4j.iocase.io.file.SearhFiles;
-
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private List<FileProperty> duplicates = new ArrayList<>();
+
+    public List<FileProperty> findDublicate() {
+        List<FileProperty> dublicate = new ArrayList<>();
+        for (int i = 0; i < duplicates.size(); i++) {
+            for (int x = i + 1; x < duplicates.size(); x++) {
+                if (duplicates.get(i).equals(duplicates.get(x))) {
+                    dublicate.add(duplicates.get(x));
+                }
+            }
+        }
+        return dublicate;
+    }
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        duplicates.add(new FileProperty(file.toFile().length(), file.toFile().getName()));
         return super.visitFile(file, attrs);
     }
 
     public static void main(String[] args) throws IOException {
+        DuplicatesVisitor dv = new DuplicatesVisitor();
         Path start = Paths.get("/home/serg/IdeaProjects/job4j_design/.idea/");
-        Files.walkFileTree(start, new DuplicatesVisitor());
-//        System.out.println(start.getParent());
+        Files.walkFileTree(start, dv);
+        dv.findDublicate().forEach(a -> System.out.println(a.getName()));
     }
 }
