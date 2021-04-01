f5a9bb2bcc9af76206103cc98fabb4d3ee3fb601
sergey <s-krasssavin@mail.ru>
2021-04-01 20:46:57 +0300

    5. Валидация параметров запуска. [#455351]

--- src/main/java/ru/job4j/iocase/io/file/Search.java
+++ src/main/java/ru/job4j/iocase/io/file/Search.java
@@ -9,8 +9,11 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("/home/serg/IdeaProjects/job4j_design/");
-        search(start, p -> p.toFile().getName().endsWith("html")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
