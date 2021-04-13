1990ecd0fa941137c487af144841a01228fd7ba5
sergey <s-krasssavin@mail.ru>
2021-04-13 22:55:59 +0300

    5.2. Архивировать проект [#455360]

---
+++ src/main/java/ru/job4j/iocase/io/Zip.java
@@ -0,0 +1,50 @@
+package ru.job4j.iocase.io;
+
+import ru.job4j.iocase.io.file.Search;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+    //java -jar pack.jar -d=/home/serg/IdeaProjects/job4j_tracker -e=class
+    // -o=/home/serg/IdeaProjects/job4j_tracker.zip
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toFile().getAbsolutePath()));
+
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(String.valueOf(path)))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Search search = new Search();
+        List<Path> result = search.search(Paths.get("/home/serg/IdeaProjects/job4j_tracker"),
+                p -> !p.toFile().getName().endsWith("class"));
+        new Zip().packFiles(result, new File("/home/serg/IdeaProjects/job4j_tracker.zip"));
+//        new Zip().packSingleFile(
+//                new File("/home/serg/IdeaProjects/job4j_tracker"),
+//                new File("/home/serg/IdeaProjects/job4j_tracker.zip")
+//        );
+    }
+}
