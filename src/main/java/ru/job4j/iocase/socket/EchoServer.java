29afe846e2f3c105fdb3e49d382634a76c144fd8
sergey <s-krasssavin@mail.ru>
2021-05-16 14:45:27 +0300

    4. Slf4j - вывод exception. [#455347]

--- src/main/java/ru/job4j/iocase/socket/EchoServer.java
+++ src/main/java/ru/job4j/iocase/socket/EchoServer.java
@@ -3,10 +3,17 @@ package ru.job4j.iocase.socket;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.iocase.logging.UsageLog4j;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+//        try (ServerSocket server = new ServerSocket(9000)) {
+        try (ServerSocket server = new ServerSocket()) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
@@ -33,6 +40,8 @@ public class EchoServer {
                     }
                 }
             }
+        }catch (Exception e) {
+            LOG.error("Check connection parameters", e);
         }
     }
 }
