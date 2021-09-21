f308dd73a6d3ff10454b194f16d614fceeb0566e
skrasavin <s-krasssavin@mail.ru>
2021-09-22 01:34:57 +0300

    4.2. Поиск дубликатов [#315066 #88277]

--- src/main/java/ru/job4j/iocase/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/iocase/io/duplicates/DuplicatesVisitor.java
@@ -4,42 +4,17 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-//    private List<FileProperty> duplicates = new ArrayList<>();
-    private Set<FileProperty> duplicates = new LinkedHashSet<>();
+    private final List<FileProperty> duplicates = new ArrayList<>();
 
     public Set<FileProperty> findDublicate() {
-        Set<FileProperty> dublicate = new LinkedHashSet<>();
-        for (FileProperty s : duplicates) {
-            System.out.println(s.getAbsolutePath());
-        }
-
-
-//        List<FileProperty> dublicate = new ArrayList<>();
-//        for (int i = 0; i < duplicates.size(); i++) {
-//            for (int x = i + 1; x < duplicates.size(); x++) {
-//                if (duplicates.get(i).equals(duplicates.get(x))) {
-//                    dublicate.add(duplicates.get(i));
-//                    dublicate.add(duplicates.get(x));
-//                }
-//            }
-//        }
-        return dublicate;
+        Set<FileProperty> dublicate = new HashSet<>();
+        return duplicates.stream()
+                .filter(e -> !dublicate.add(e))
+                .collect(Collectors.toSet());
     }
-
-//    public List<FileProperty> findDublicate() {
-//        List<FileProperty> dublicate = new ArrayList<>();
-//        for (int i = 0; i < duplicates.size(); i++) {
-//            for (int x = i + 1; x < duplicates.size(); x++) {
-//                if (duplicates.get(i).equals(duplicates.get(x))) {
-//                    dublicate.add(duplicates.get(i));
-//                    dublicate.add(duplicates.get(x));
-//                }
-//            }
-//        }
-//        return dublicate;
-//    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
@@ -51,10 +26,24 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor dv = new DuplicatesVisitor();
-        Path start = Paths.get("/home/serg/IdeaProjects/job4j_design/.idea/");
+        Path start = Paths.get("C:/Users/sskra/IdeaProjects/job4j_tracker_test");
         Files.walkFileTree(start, dv);
         dv.findDublicate()
                 .forEach(
                 a -> System.out.println(a.getName() + " - " + a.getAbsolutePath()));
     }
 }
+
+
+//    public List<FileProperty> findDublicate1() {
+//        List<FileProperty> dublicate = new ArrayList<>();
+//        for (int i = 0; i < duplicates.size(); i++) {
+//            for (int x = i + 1; x < duplicates.size(); x++) {
+//                if (duplicates.get(i).equals(duplicates.get(x))) {
+//                    dublicate.add(duplicates.get(i));
+//                    dublicate.add(duplicates.get(x));
+//                }
+//            }
+//        }
+//        return dublicate;
+//    }
