4c071f9a4389955eea07f4cb39d62b783618079b
skrasavin <s-krasssavin@mail.ru>
2021-11-21 19:45:00 +0100

    5. Очередь на двух стеках [#160 #222631]

--- src/main/java/ru/job4j/list/SimpleQueue.java
+++ src/main/java/ru/job4j/list/SimpleQueue.java
@@ -2,13 +2,6 @@ package ru.job4j.list;
 
 import java.util.NoSuchElementException;
 
-/**
- * код класса можно упростить, для этого оставьте в методе push
- * только помещение элемента в стек in. В методе poll надо сначала проверить,
- * что если в стеках нет переменных, то выбросить соответствующее исключение.
- * Потом, если стек out пуст, то переместите из стека in все элементы в стек out
- * @param <T>
- */
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
@@ -28,14 +21,4 @@ public class SimpleQueue<T> {
     public void push(T value) {
         in.push(value);
     }
-
-//    public void push(T value) {
-//        while (outCount > inCount) {
-//            in.push(out.pop());
-//            inCount++;
-//            outCount--;
-//        }
-//        in.push(value);
-//        inCount++;
-//    }
 }
