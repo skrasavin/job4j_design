0e4da84249ecf5942591fc6743c3dfa61d0731b2
skrasavin <s-krasssavin@mail.ru>
2021-12-28 23:18:18 +0100

    0. Принцип единственной ответственности [#4913 #245076]

--- src/main/java/ru/job4j/ood/srp/Worker.java
+++ src/main/java/ru/job4j/ood/srp/Worker.java
@@ -1,4 +1,15 @@
 package ru.job4j.ood.srp;
 
-public class Worker extends Subclass {
+public class Worker {
+    private static Worker instance;
+
+    private Worker() {
+    }
+
+    public static Worker getInstance(String value) {
+        if (instance == null) {
+            instance = new Worker();
+        }
+        return instance;
+    }
 }
